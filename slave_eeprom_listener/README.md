963790dfb62623d86c8a6c1f3a692afbed0c3171
Juan Gutierrez <juan.gutierrez@nxp.com>
2017-10-24 19:18:04 -0500

    slave_eeprom_listener: Add sample for handling notification support
    Sample to use the notification support added to the i2c slave-eeprom backend.
    The eeprom_listener receives the i2c-bus and i2c-address of the slave-eeprom
    as an argument to register itself (by its pid) as the listener process.
    After starting the eeprom_listener will load the contents of the romfile
    (passed as a third argument) to the eeprom registers.
    After a notification from the backend driver the eeprom_listener will backup
    the content of the eeprom into the romfile.
    
    Signed-off-by: Juan Gutierrez <juan.gutierrez@nxp.com>

---
+++ slave_eeprom_listener/README.md
@@ -0,0 +1,62 @@
+## eeprom_listener
+
+Sample to use the notification support added to the i2c slave-eeprom backend.
+The eeprom_listener receives the i2c-bus and i2c-address of the slave-eeprom
+as an argument to register itself (by its pid) as the listener process.
+After starting the eeprom_listener will load the contents of the romfile
+(passed as a third argument) to the eeprom registers.
+After a notification from the backend driver the eeprom_listener will backup
+the content of the eeprom into the romfile.
+
+Usage:
+
+	./eeprom_listener <i2c-bus> <addr> <romFile> &
+
+## Build instructions
+
+	$ cd <slave_eeprom_listener>
+	$ gcc eeprom_listener.c -o eprom_listener -lrt -lpthread
+
+## Run the listener on SLAVE board
+
+A quick example for instantiating the slave-eeprom driver from userspace at
+address 0x64 on bus 1 and run the eeprom_listener to start backing up the 
+eeprom content at a romFile.
+
+
+##### 1. Instantiate the slave-eeprom backend
+
+	echo slave-24c02 0x64 > /sys/bus/i2c/devices/i2c-0/new_device
+
+##### 2. Start the listener
+
+	./eeprom_listener i2c-0 0x64 romFile &
+
+##### 3. On the master device start some reads/writes to the i2c device
+
+ On the MASTER device 
+
+  - Dump the content of the eeprom
+
+	$ i2cdump -y 0 0x64
+
+  - Write the date at address 0xC0
+ 
+	$ date > dateFile
+	$ ./eeprog -i dateFile /dev/i2c-0 0x64 -w 0xc0
+
+ - Read the contents again
+	
+	$ i2cdump -y 0 0x64
+
+ On the SLAVE device
+
+ You will see some logs like:
+
+	Backup eeprom content on file...
+
+## Kill the process
+
+You can kill the listener by name
+
+	pkill eeprom_listener
---
+++ slave_eeprom_listener/eeprom_listener.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define SIG_TEST 44
+sem_t writeSem;
+
+/* register_pid()
+ * Function to register the process id with the i2c-slave notifier.
+ * The proc_id is exposed on the i2c device path.
+ * Writing the pid to this sysfs register the process to be notified
+ */
+void register_pid(int pid, char *bus, int addr) {
+  int fd;
+  int num;
+  char str[100];
+
+  num = sprintf(str, "/sys/bus/i2c/devices/%s/0-00%x/proc_id", bus, addr);
+  fd = open(str, O_WRONLY);
+  
+  num = sprintf(str, "%d", pid);
+  write (fd, str, num);
+  close(fd);
+}
+
+/* load_eeprom_from_file()
+ * Load the contents of a file (filename) into the simulated eeprom
+ * Since the eeprom is simulated by the slave-eeprom backend driver
+ * it starts with zeros at probe time. To keep the record a backup
+ * file can be stored on the filesystem and initialize the eeprom
+ * with this backup-file
+ */
+#define EEPROM_SIZE 0x100
+void load_eeprom_from_file(char* filename, char *bus, int addr) {
+  FILE *fd_device;
+  FILE *fd_file;
+  int i;
+  char ch;
+  char str[100];
+
+  sprintf(str, "/sys/bus/i2c/devices/%s/0-00%x/slave-eeprom", bus, addr);
+  fd_device = fopen(str, "w");
+  fd_file = fopen(filename, "r");
+ 
+  printf("Loading eeprom values\n");
+  for (i = 0; i < EEPROM_SIZE; i ++) {
+    ch = fgetc(fd_file);
+    fputc(ch, fd_device);
+  }
+
+  fclose(fd_file);
+  fclose(fd_device);
+}
+
+/* backup_eeprom_on_file()
+ * This function updates the contents of the backup-file, so it 
+ * mirror the slave-eeprom content and this can be restored at any
+ * time if needed or after restarting the system.
+ */
+
+void backup_eeprom_on_file(char* filename, char *bus, int addr) {
+  FILE *fd_device;
+  FILE *fd_file;
+  int i;
+  char ch;
+  char str[100];
+
+  sprintf(str, "/sys/bus/i2c/devices/%s/0-00%x/slave-eeprom", bus, addr);
+  fd_device = fopen(str, "r");
+  fd_file = fopen(filename, "w");
+
+  printf("Backup eeprom content on file...\n");
+  for (i = 0; i < EEPROM_SIZE; i ++) {
+    ch = fgetc(fd_device);
+    fputc(ch, fd_file);
+  }
+
+  fclose(fd_file);
+  fclose(fd_device);
+}
+
+/* signalFunction()
+ * Callback function called when kernel driver signal the STOP event
+ * Since the STOP event is called multiple times, the very first time
+ * the callback sleeps to give a chance the I2C TX/RX event finishes
+ * and let the rest of the signals (STOP events) arrive all at once.
+ * Posting the semaphores after the TX/RX has finished allow to only
+ * unlock and attend  once the backup_eeprom_on_file routine
+ * instead of multiple times during the middle of the TX/RX event.
+ */
+void signalFunction(int n, siginfo_t *info, void *unused) {
+  int count;
+  sem_getvalue(&writeSem, &count);
+
+  /* Sleep 1 sec when receiving the very first signal     
+   * to allow the TX/RX finishes. After that we can post  
+   * semaphore to backup the eeprom content.
+   */
+
+  if (!count)
+    sleep(1); /* Wait for the I2C TX/RX to finish */
+  sem_post(&writeSem);
+     
+}
+
+void print_usage(int argc, char *argv[])
+{
+  printf("Usage: \n\n");
+  printf("  %s i2c-<bus> <address> <romfile> & \n\n", argv[0]);
+  printf("Example for a device at addr=0x64 on the 12c-1 bus with storage file named romFile: \n\n");
+  printf("  %s i2c-1 0x64 romFile & \n\n", argv[0]);
+}
+
+void main(int argc, char *argv[]){
+  int pid;
+  int count;
+  char *bus;
+  char* filename;
+  int addr;
+  struct sigaction sig;
+
+  /* Minimal error-checking added */
+  if (argc < 4) {
+    print_usage(argc, argv);
+    return;
+ }
+
+  bus = argv[1];
+  addr = strtol(argv[2], NULL, 0);
+  filename = argv[3];
+
+  sem_init(&writeSem, 0, 0);
+
+  /* Registering this process with the i2c-slave backend by pid */
+  pid = getpid();
+  register_pid(pid, bus, addr);
+
+  /* Register the callback that attends the notification */
+  sig.sa_sigaction = signalFunction;
+  sig.sa_flags = SA_SIGINFO;
+  sigaction(SIG_TEST, &sig, NULL);
+
+  /* Load eeprom register from the backup-file */
+  load_eeprom_from_file(filename, bus, addr);
+
+  printf("Starting the listener loop...\n");
+  while(1)
+  {
+    /* Pending for notifications to arrive*/
+    sem_wait(&writeSem);
+    sem_getvalue(&writeSem, &count);
+    /* Only until no more posts are pending backup the file */
+    if (!count) {
+      backup_eeprom_on_file(filename, bus, addr);
+    }
+  }
+
+}
